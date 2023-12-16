8d533fa5c36b744b690e88d22dc31adb3ec475d3
Albert731006 <151415123+Albert731006@users.noreply.github.com>
2023-12-16 14:24:43 +0800

    modify

--- pico_w/pico10_111.py
+++ pico_w/pico10_111.py
@@ -1,10 +1,14 @@
 from machine import ADC,Timer
 
+def alarm():
+    print("要爆炸了")
 def callback1(t:Timer):
     sensor = ADC(4)
     vol=sensor.read_u16()*(3.3/65535)
     temperature=27-(vol-0.706) / 0.001721
     print(temperature)
+    if temperature >=28:
+        alarm() 
 
 time1 = Timer()
 time1.init(period=1000,callback=callback1)
