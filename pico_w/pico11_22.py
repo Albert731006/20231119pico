d64101b65fcf5eb24df60c7549389e0ab25dc44e
Albert731006 <151415123+Albert731006@users.noreply.github.com>
2024-01-06 15:24:33 +0800

    modify

---
+++ pico_w/pico11_22.py
@@ -0,0 +1,62 @@
+import network
+import time
+from machine import WDT,Timer,ADC
+import urequests as requests
+
+def connect():
+    # enable station interface and connect to WiFi access point
+    nic = network.WLAN(network.STA_IF)
+    nic.active(True)
+    nic.connect('yioyioman', '0912886806')
+
+    max_wait = 10
+
+    #處理正在連線
+    while max_wait > 0:
+        max_wait -= 1
+        status = nic.status()
+        if status < 0 or status >=3:
+            break
+        print("等待連線")
+        time.sleep(1)
+
+
+        
+    #沒有wifi的處理
+    if nic.status() != 3:
+        #連線失敗,重新開機
+        #wdt = WDT(timeout=2000)
+        #wdt.feed()
+        raise RuntimeError('連線失敗')
+        
+    else:
+        print("成功連線")
+        print(nic.ifconfig())
+  
+
+def alert():
+    print('要爆炸了!')
+    response = requests.get('https://hook.eu2.make.com/vm1owivb4sil9rmw4ad4at65t5f8cz4b?name=changcyy&date=2024-01-06&temperature=29.999')
+    print(help(response))
+    response.close()
+    
+def callback1(t:Timer):
+    global start
+    sensor = ADC(4)    
+    vol = sensor.read_u16() * (3.3/65535)
+    temperature = 27 - (vol-0.706) / 0.001721
+    print(f'溫度:{temperature}')    
+    delta = time.ticks_diff(time.ticks_ms(), start)
+    print(delta)
+    #溫度超過24度,並且發送alert()的時間已經大於60秒
+    if temperature >= 24 and delta >= 60 * 1000:        
+        alert()
+        start = time.ticks_ms()#重新設定計時的時間
+
+connect()
+
+start = time.ticks_ms() - 60 * 1000 #應用程式啟動時,計時時間,先減60秒
+    
+time1 = Timer()
+time1.init(period=1000,callback=callback1)
+
