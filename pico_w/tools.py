380eeff094576ad0856d1f5edbe76fc8fe132533
Albert731006 <151415123+Albert731006@users.noreply.github.com>
2024-01-27 08:52:19 +0800

    update

--- pico_w/tools.py
+++ pico_w/tools.py
@@ -7,8 +7,8 @@ from machine import WDT
 rp2.country('TW')
 
 
-ssid = 'home'
-password = '035513659'
+ssid = 'yioyioman'
+password = '0912886806'
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
