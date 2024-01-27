cbdaac269a9d95cd6bba359df291219a8065ea5c
Albert731006 <151415123+Albert731006@users.noreply.github.com>
2024-01-27 14:37:32 +0800

    update

--- pico_w/pico14_1.py
+++ pico_w/pico14_1.py
@@ -6,13 +6,13 @@ def fun10(t:Timer | None = None):
     print('10秒了')
     led.toggle()
 
-def fun10ms(t:Timer | None = None):
+def fun500ms(t:Timer | None = None):
     print(light.read_u16())    
 
 led = Pin(15, Pin.OUT)
 light = ADC(Pin(28))
 timer10 = Timer(period=10000, mode=Timer.PERIODIC, callback=fun10)
-timer10ms = Timer(period=100, mode=Timer.PERIODIC, callback=fun10ms)
+timer500ms = Timer(period=500, mode=Timer.PERIODIC, callback=fun500ms)
 fun10()
 
     
