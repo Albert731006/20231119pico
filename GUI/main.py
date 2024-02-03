fd9d65f4b074a1dcfb5cc03cc74188c1ba49bcaf
Albert731006 <151415123+Albert731006@users.noreply.github.com>
2024-02-03 07:14:34 +0000

    modify

--- GUI/main.py
+++ GUI/main.py
@@ -9,7 +9,7 @@ st.title("Pico_W職能發展協會專案")
 st.header("雞舍:red[溫度]和:blue[光線]狀態")
 st.divider()
 
-st.snow()
+
 
 url = 'https://blynk.cloud/external/api/get?token=-KbMBBGu5P410XNnXqs-e18vc24DiAOU&v0&v1'
 
